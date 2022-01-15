812dfbc9e51d34495a35b7ae594a057479e81e68
stefanie18 <89417960+stefanie18@users.noreply.github.com>
2022-01-14 20:00:10 -0600

    Add files via upload

---
+++ console.js
@@ -0,0 +1,13 @@
+const a = "10";
+const b = "5";
+
+console.log(`<p>${a} ${b}`);
+
+const firstName = "Stef";
+const lastName = "Perez";
+console.log(`<p>${firstName} ${lastName}`);
+
+
+const x = "x";
+const y = "y";
+console.log(`<p>${x} ${b}`);
